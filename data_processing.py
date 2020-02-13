251888543cb2278e7aa036742f36a3190e3839c8
Simon Murphy <simonmurphy66@hotmail.com>
2020-02-13 15:13:03 +1100

    Files for processing the data and making the bokeh plot.

---
+++ data_processing.py
@@ -0,0 +1,156 @@
+import glob
+import lightkurve as lk
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tqdm import tqdm_notebook as tqdm
+
+github = "https://raw.githubusercontent.com/SimonJMurphy/TESSSB9/master"
+
+# Read the SB9 data tables
+maindata = pd.read_csv(f"{github}/data/SB9public/Main.dta", names=["SysNum","J1900","J2000","component","mag1","filter1","mag2","filter2","spt1","spt2"], delimiter='|')
+
+names = ["SysNum", "OrbNum", "Porb", "Porb_err", "tp", "tp_err", "tp_flag", "ecc", "ecc_err", "omega", "omega_err", "K1", "K1_err", "K2", "K2_er", "gamma", "gamma_err", "rmsRV1", "rmsRV2", "numRV1", "numRV2", "Grade", "Bibcode", "Contributor", "Accessibility", "JDRef"]
+orbits = pd.read_csv(f"{github}/data/SB9public/Orbits.dta",names=names, delimiter="|")
+
+# Read Kareem's SB9+TESS cross-match
+df = pd.read_csv(f"{github}/data/sb9_x_gaia_with_tess_sectors.csv")
+
+# find orbits with fewest null values. 
+### Should probably replace with the orbit with most measured RVs. ###
+xorbs = orbits.loc[orbits.notnull().sum(1).groupby(orbits.SysNum).idxmax()]
+
+# merge our TIC-matched df with the good orbits (kind of weird, but we don't want duplicates)
+xdf = df.merge(xorbs,left_on="sb9_id",right_on='SysNum')
+
+
+# Loop over tiles and find unique TICS. This requires connection to silo2 server
+print("Looping over tiles to find unique TICs. Server activity can take a while...")
+files = []
+for sector in list(range(1, 20)):
+    files.extend(
+        glob.glob("/Volumes/silo2/dhey3294/TESS/sector_" + str(sector) + "/tess*.fits")
+    )
+tics = [a.split("_")[1].split("/")[-1].split("-")[2].lstrip("0") for a in files]
+
+# rescue with the online files if silo2 is out of action
+if len(tics) < 1:
+    files.extend(
+            glob.glob(f"{github}/data/TESS_plots/LKs/*png")
+        )
+    tics = [a.split("/")[-1].split(".")[0] for a in files]
+
+unique_tics = np.unique(tics)
+
+
+# keep only the systems we have a TESS LK of.
+tf = xdf[xdf["TIC"].isin(unique_tics)]
+
+# We mostly want to use log p, and we also want abs mag and the number of RVs
+tf["logp"] = np.log10(tf["p"])
+tf['absmag'] = tf['phot_g_mean_mag'].values - 5 * np.log10(1/(tf['parallax'].values)*1000) + 5
+numRVs = []
+for rv1,rv2 in zip(tf['numRV1'].values,tf['numRV2'].values):
+    if np.isnan(rv1) and np.isnan(rv2):
+        numRVs.append(-1)
+    else:
+        numRVs.append(np.nanmax([rv1,rv2]))
+tf["numRVs"] = numRVs
+
+# construct filename dataframe
+fts = [f"{github}/data/TESS_plots/FTs/{ticid}.png" for ticid in tf["TIC"].values]
+lks = [f"{github}/data/TESS_plots/LKs/{ticid}.png" for ticid in tf["TIC"].values]
+scaled_fts = [f"{github}/data/TESS_plots/scaled_FTs/{ticid}.png" for ticid in tf["TIC"].values]
+folded_lks = [f"{github}/data/TESS_plots/folded_LKs/{ticid}.png" for ticid in tf["TIC"].values]
+tic_pg = pd.DataFrame({
+    'ID': tf["TIC"].values,
+    'fts': fts,
+    'lks': lks,
+    'scaled_fts': scaled_fts,
+    'folded_lks': folded_lks
+})
+
+# overwrite the dataframe to include these filenames
+tf = tf.merge(tic_pg,left_on="TIC",right_on='ID')
+
+# write this df to csv
+tf.to_csv("plot_df.csv",index=False)
+
+
+## Various plotting functions
+
+# read a lightcurve into memory
+def get_lightcurve(unique_tic):
+    unique_tic = str(unique_tic)
+    indices = [i for i, x in enumerate(tics) if x == unique_tic]
+    lc = lk.TessLightCurveFile(files[indices[0]]).PDCSAP_FLUX.normalize()
+    for index in indices[1:]:
+        lc = lc.append(lk.TessLightCurveFile(files[index]).PDCSAP_FLUX.normalize())
+    lc = lc.remove_nans()
+    return lc
+
+def produce_lks(ticnum):
+    lc = get_lightcurve(ticnum)
+    fig = lc.plot()
+    plt.savefig(f"{github}/data/TESS_plots/LKs/{ticnum}.png",bbox_inches='tight')
+    plt.clf()
+    plt.close('all')
+
+def produce_folded_lks(ticnum):
+    lc = get_lightcurve(ticnum)
+    p = tf.query(f"TIC=={ticnum}")["p"].values[0]
+    fig = lc.fold(period=p).plot()
+    plt.savefig(f"{github}/data/TESS_plots/folded_LKs/{ticnum}.png",bbox_inches='tight')
+    plt.clf()
+    plt.close('all')
+
+def produce_fts(ticnum):
+    p = tf.query(f"TIC=={ticnum}")["p"].values[0]
+    fmax = 15
+    lc = get_lightcurve(ticnum)
+    fig = lc.to_periodogram(maximum_frequency=fmax).plot()
+    if 1/p < fmax:
+        plt.axvline(1.0/p,color='r', ls='--', lw=0.8)
+    plt.xlim(0,fmax)
+    plt.ylim(bottom=0)
+    plt.savefig(f"{github}/data/TESS_plots/FTs/{ticnum}.png",bbox_inches='tight')
+    plt.clf()
+    plt.close('all')
+
+def produce_scaled_fts(ticnum):
+    p = tf.query(f"TIC=={ticnum}")["p"].values[0]
+    if p < 10:
+        fmax = 8/p
+    else:
+        fmax = 10
+    lc = get_lightcurve(ticnum)
+    fig = lc.to_periodogram(maximum_frequency=fmax).plot()
+    plt.axvline(1.0/p,color='r', ls='--', lw=0.8)
+    plt.xlim(0,fmax)
+    plt.ylim(bottom=0)
+    plt.savefig(f"{github}/data/TESS_plots/scaled_FTs/{ticnum}.png",bbox_inches='tight')
+    plt.clf()
+    plt.close('all')
+
+# (Re-)Produce the plots if the variable below is set to True
+produce_plots = False
+if produce_plots:
+  print("Producing plots...")
+  for t in tqdm(tf["TIC"].values):
+      try:
+          produce_lks(t)
+      except:
+          print(f"TIC{t} lk plot failed.")
+      try:
+          produce_fts(t)
+      except:
+          print(f"TIC{t} ft plot failed.")
+      try:
+          produce_scaled_fts(t)
+      except:
+          print(f"TIC{t} scaled ft plot failed.")
+      try:
+          produce_folded_lks(t)
+      except:
+          print(f"TIC{t} folded lk plot failed.")
+
---
+++ make_bokeh_plot.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.io import show, output_notebook
+import bokeh
+from bokeh.plotting import figure, output_file, show, ColumnDataSource
+from bokeh.models import HoverTool,  WheelZoomTool, ColorBar
+from bokeh.palettes import Viridis
+from bokeh.transform import linear_cmap
+
+tf = pd.read_csv("plot_df.csv")
+
+output_file("index.html", title='TESSSB9')
+source = ColumnDataSource(
+        data=dict(
+            x=tf['logp'].values,
+            y=tf['e'].values,
+            fts = tf['fts'].values,
+            lks = tf['lks'].values,
+            scaled_fts = tf['scaled_fts'].values,
+            folded_lks = tf['folded_lks'].values,
+            TIC = tf["TIC"].values,
+            color = tf["numRVs"].values
+        )
+    )
+hover = HoverTool(# height="200" alt="@imgs" width="500"
+        tooltips="""
+        <div>
+            <div>
+                <img
+                    src="@lks" alt="lks" width="420"
+                    border="2"
+                ></img>
+            </div>
+            <div>
+                <img
+                    src="@scaled_fts" alt="scaled_fts" width="420"
+                    border="2"
+                ></img>
+            </div>
+            <div>
+                <span style="font-size: 17px; font-weight: bold;">TIC @TIC</span>
+            </div>
+        </div>
+        """
+    )
+wheel = WheelZoomTool()
+p = figure(#plot_width=900, plot_height=900, 
+           tools=[hover, 'zoom_in', 'undo', wheel, 'reset', 'box_zoom'],
+           title="TESSSB9", 
+           toolbar_location="above",
+           sizing_mode='stretch_both',
+           x_range=(-1,5), y_range=(0,1),
+            output_backend="webgl"
+          )
+
+mapper = linear_cmap(
+    field_name="color",
+    palette=bokeh.palettes.Viridis256,
+    low=0,
+    high=20,
+)
+
+p.circle('x', 'y',radius=0.01,fill_alpha=0.6,source=source,line_color=mapper,color=mapper)
+p.xaxis.axis_label = 'log period, d'
+p.yaxis.axis_label = 'eccentricity'
+p.xaxis.axis_label_text_font_size = "18pt"
+p.yaxis.axis_label_text_font_size = "18pt"
+p.xaxis.major_label_text_font_size = "15pt"
+p.yaxis.major_label_text_font_size = "15pt"
+p.title.text_font_size = "15pt"
+p.xaxis.axis_label_text_font_style = 'normal'
+p.yaxis.axis_label_text_font_style = 'normal'
+p.toolbar.active_scroll=wheel
+color_bar = ColorBar(color_mapper=mapper["transform"], width=8, location=(0, 0))
+p.add_layout(color_bar, "right")
+show(p)
